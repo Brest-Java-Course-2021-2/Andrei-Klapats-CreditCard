af912118d1de83a071f0008fb0c11a01245026c9
KlapotA <klapotandrey93@gmail.com>
2021-12-20 08:23:16 +0300

    Add Account's dao-api, dao implementation (with default realization)

---
+++ dao-api/src/main/java/com/epam/brest/dao/AccountDao.java
@@ -0,0 +1,25 @@
+package com.epam.brest.dao;
+
+import com.epam.brest.model.Account;
+import java.util.Date;
+import java.util.List;
+
+public interface AccountDao {
+
+    List<Account> findAll();
+
+    Account getAccountByClientId(Integer clientId);
+
+    Account getAccountByAccountId(Integer accountId);
+
+    Account getAccountByDateOfCreate(Date dateOfCreate);
+
+    Integer create(Account account);
+
+    Integer update(Account account);
+
+    Integer delete(Integer accountId);
+
+    Integer count();
+
+}
---
+++ dao/src/main/java/com/epam/brest/dao/AccountDaoJDBSImpl.java
@@ -0,0 +1,48 @@
+package com.epam.brest.dao;
+
+import com.epam.brest.model.Account;
+
+import java.util.Date;
+import java.util.List;
+
+public class AccountDaoJDBSImpl implements AccountDao{
+    @Override
+    public List<Account> findAll() {
+        return null;
+    }
+
+    @Override
+    public Account getAccountByClientId(Integer clientId) {
+        return null;
+    }
+
+    @Override
+    public Account getAccountByAccountId(Integer accountId) {
+        return null;
+    }
+
+    @Override
+    public Account getAccountByDateOfCreate(Date dateOfCreate) {
+        return null;
+    }
+
+    @Override
+    public Integer create(Account account) {
+        return null;
+    }
+
+    @Override
+    public Integer update(Account account) {
+        return null;
+    }
+
+    @Override
+    public Integer delete(Integer accountId) {
+        return null;
+    }
+
+    @Override
+    public Integer count() {
+        return null;
+    }
+}
